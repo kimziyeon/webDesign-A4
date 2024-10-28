b4fb9a523e0514014c74ea7b324a2706f3ff0050
kimziyeon <aa40254037@gmail.com>
2024-10-28 16:01:49 +0900

    20241028

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
 $(function () {
-    // alert('연결왼료')
+
+    $('.popup_on').click(function () {
+        $('.popup').show();
+    })
+
+    $('.popup_off').click(function () {
+        $('.popup').hide();
+    })
+
+
+
+    $('header nav').mouseenter(function () {
+        $('.sub').stop().slideDown();
+        $('.nav_back').stop().slideDown();
+    })
+
+    $('header nav').mouseleave(function () {
+        $('.sub').stop().slideUp();
+        $('.nav_back').stop().slideUp();
+    })
+
+    $('.sub').mouseenter(function () {
+        $(this).parents('li').children('a').addClass('on')
+    })
+
+    $('.sub').mouseleave(function () {
+        $(this).parents('li').children('a').removeClass('on')
+    })
+
+
+
+    $('.gall ul li>a>img').mouseenter(function () {
+        $(this).css('opacity', '0.5');
+    })
+
+    $('.gall ul li>a>img').mouseleave(function () {
+        $(this).css('opacity', '1');
+    })
+
+
+    var s = 0;
+    function slide() {
+
+        if (s < 2) {
+            s++;
+        } else {
+            s = 0;
+        }
+        $('.slide ul li').fadeOut();
+        $('.slide ul li').eq(s).fadeIn();
+
+    }
+    setInterval(slide, 2000);
 })
